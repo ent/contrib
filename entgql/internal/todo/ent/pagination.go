fbad371d68b46c30d464531a57d95f6f9ff1d2b4
Giau. Tran Minh <giau.tmg@gmail.com>
2020-12-13 15:01:19 +0000

    fix: run generate

--- entgql/internal/todo/ent/pagination.go
+++ entgql/internal/todo/ent/pagination.go
@@ -550,12 +550,11 @@ var DefaultTodoOrder = &TodoOrder{
 	},
 }
 
-// ToEdge return new TodoEdge
+// ToEdge converts Todo into TodoEdge.
 func (t *Todo) ToEdge(order *TodoOrder) *TodoEdge {
 	if order == nil {
 		order = DefaultTodoOrder
 	}
-
 	return &TodoEdge{
 		Node:   t,
 		Cursor: order.Field.toCursor(t),
