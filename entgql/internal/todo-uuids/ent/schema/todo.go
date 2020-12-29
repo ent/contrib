7b6ef8e026d71cd9b348b46e11343faaabc7e95b
Travis Cline <travis.cline@gmail.com>
2020-12-28 20:43:43 -0800

    examples: Add todo-uuids example that uses UUIDs for ids

---
+++ entgql/internal/todo-uuids/ent/schema/todo.go
@@ -0,0 +1,73 @@
+// Copyright 2019-present Facebook
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package schema
+
+import (
+	"time"
+
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+	"github.com/facebookincubator/ent-contrib/entgql"
+	"github.com/google/uuid"
+)
+
+// Todo defines the todo type schema.
+type Todo struct {
+	ent.Schema
+}
+
+// Fields returns todo fields.
+func (Todo) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New),
+		field.Time("created_at").
+			Default(time.Now).
+			Immutable().
+			Annotations(
+				entgql.OrderField("CREATED_AT"),
+			),
+		field.Enum("status").
+			NamedValues(
+				"InProgress", "IN_PROGRESS",
+				"Completed", "COMPLETED",
+			).
+			Annotations(
+				entgql.OrderField("STATUS"),
+			),
+		field.Int("priority").
+			Default(0).
+			Annotations(
+				entgql.OrderField("PRIORITY"),
+			),
+		field.Text("text").
+			NotEmpty().
+			Annotations(
+				entgql.OrderField("TEXT"),
+			),
+	}
+}
+
+// Edges returns todo edges.
+func (Todo) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("children", Todo.Type).
+			Annotations(entgql.Bind()).
+			From("parent").
+			Annotations(entgql.Bind()).
+			Unique(),
+	}
+}
