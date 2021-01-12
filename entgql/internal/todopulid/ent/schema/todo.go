cc5b26154ecb10fb65fba34ad0f8de0fd5f38e50
Ariel Mashraki <ariel@mashraki.co.il>
2021-01-12 09:24:19 +0200

    examples/pulid: reuse base todo schema

--- entgql/internal/todopulid/ent/schema/todo.go
+++ entgql/internal/todopulid/ent/schema/todo.go
@@ -15,12 +15,8 @@
 package schema
 
 import (
-	"time"
-
 	"github.com/facebook/ent"
-	"github.com/facebook/ent/schema/edge"
-	"github.com/facebook/ent/schema/field"
-	"github.com/facebookincubator/ent-contrib/entgql"
+	"github.com/facebookincubator/ent-contrib/entgql/internal/todo/ent/schema"
 	"github.com/facebookincubator/ent-contrib/entgql/internal/todopulid/ent/schema/pulid"
 )
 
@@ -32,47 +28,9 @@ type Todo struct {
 // Mixin returns todo mixed-in schema.
 func (Todo) Mixin() []ent.Mixin {
 	return []ent.Mixin{
-		pulid.MixinWithPrefix("TD"), // "TD" declared once.
-	}
-}
-
-// Fields returns todo fields.
-func (Todo) Fields() []ent.Field {
-	return []ent.Field{
-		field.Time("created_at").
-			Default(time.Now).
-			Immutable().
-			Annotations(
-				entgql.OrderField("CREATED_AT"),
-			),
-		field.Enum("status").
-			NamedValues(
-				"InProgress", "IN_PROGRESS",
-				"Completed", "COMPLETED",
-			).
-			Annotations(
-				entgql.OrderField("STATUS"),
-			),
-		field.Int("priority").
-			Default(0).
-			Annotations(
-				entgql.OrderField("PRIORITY"),
-			),
-		field.Text("text").
-			NotEmpty().
-			Annotations(
-				entgql.OrderField("TEXT"),
-			),
-	}
-}
-
-// Edges returns todo edges.
-func (Todo) Edges() []ent.Edge {
-	return []ent.Edge{
-		edge.To("children", Todo.Type).
-			Annotations(entgql.Bind()).
-			From("parent").
-			Annotations(entgql.Bind()).
-			Unique(),
+		// "TD" declared once.
+		pulid.MixinWithPrefix("TD"),
+		// Reuse the fields and edges from base example.
+		schema.Todo{},
 	}
 }
