954941ae8dc4fac9ae1191f80b25f30b25be0118
Rotem Tamir <rotemtamir@gmail.com>
2021-03-13 12:40:25 +0200

    entproto/cmd/protoc-gen-entgrpc - generate a skeleton service implementation using a protoc plugin

--- entproto/internal/todo/ent/proto/entpb/generate.go
+++ entproto/internal/todo/ent/proto/entpb/generate.go
@@ -14,4 +14,4 @@
 
 package entpb
 
-//go:generate protoc -I=.. --go_out=.. --go-grpc_out=.. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative entpb/entpb.proto
+//go:generate protoc -I=.. --go_out=.. --go-grpc_out=.. --go_opt=paths=source_relative --entgrpc_out=.. --entgrpc_opt=paths=source_relative,schema_path=../../schema --go-grpc_opt=paths=source_relative entpb/entpb.proto
