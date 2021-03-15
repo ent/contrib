6fdc82e721f82cf4fd6a3511f0f53efe711255f9
Rotem Tamir <rotemtamir@gmail.com>
2021-03-15 15:22:08 +0200

    entproto - adding FieldMap - used later in the gRPC codegen to easily describe the mapping between the ent.Schema fields and the proto desc.MessageDescritpor

---
+++ entproto/internal/entprototest/fieldmapper_test.go
@@ -0,0 +1,37 @@
+// Copyright 2019-present Facebook
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package entprototest
+
+func (suite *AdapterTestSuite) TestFieldMap() {
+	mp, err := suite.adapter.FieldMap("User")
+	suite.Require().NoError(err)
+	suite.Require().NotNil(mp)
+
+	userName, ok := mp["user_name"]
+	suite.Require().True(ok)
+	suite.Assert().False(userName.IsIDField)
+	suite.Assert().False(userName.IsEdgeField)
+	suite.Assert().NotNil(userName.EntField)
+	suite.Assert().NotNil(userName.PbFieldDescriptor)
+	suite.Assert().NotNil(userName.PbFieldDescriptor)
+	suite.Assert().NoError(err)
+	suite.Assert().EqualValues("UserName", userName.PbStructField())
+
+	id, ok := mp["id"]
+	suite.Require().True(ok)
+	suite.Assert().True(id.IsIDField)
+	suite.Assert().False(id.IsEdgeField)
+	suite.Assert().EqualValues("Id", id.PbStructField())
+}
