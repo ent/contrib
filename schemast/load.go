9f42aad801a31147ffd9224e8149ca377cc7ecc0
Rotem Tamir <rotemtamir@gmail.com>
2021-05-05 15:34:01 +0300

    Update schemast/load.go
    Co-authored-by: Ariel Mashraki <7413593+a8m@users.noreply.github.com>

--- schemast/load.go
+++ schemast/load.go
@@ -36,8 +36,10 @@ func (c *Context) HasType(typeName string) bool {
 
 func (c *Context) lookupTypeDecl(typeName string) (*ast.File, *ast.GenDecl, bool) {
 	for _, file := range c.syntax() {
-		var found *ast.GenDecl
-		var parent *ast.File
+		var (
+			found *ast.GenDecl
+			parent *ast.File
+		)
 		ast.Inspect(file, func(node ast.Node) bool {
 			if decl, ok := node.(*ast.GenDecl); ok {
 				if isTypeDeclFor(decl, typeName) {
