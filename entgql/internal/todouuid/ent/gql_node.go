ad5089ae468ebdecb814dafc9df7c26b93bd8be1
Ariel Mashraki <7413593+a8m@users.noreply.github.com>
2021-08-19 17:16:17 +0300

    entgql: add gql prefix for generated files (#125)
    Closed https://github.com/ent/ent/issues/1408

--- entgql/internal/todouuid/ent/gql_node.go
+++ entgql/internal/todouuid/ent/gql_node.go
@@ -252,7 +252,6 @@ func (c *Client) noder(ctx context.Context, table string, id uuid.UUID) (Noder, 
 	case category.Table:
 		n, err := c.Category.Query().
 			Where(category.ID(id)).
-			CollectFields(ctx, "Category").
 			Only(ctx)
 		if err != nil {
 			return nil, err
@@ -261,7 +260,6 @@ func (c *Client) noder(ctx context.Context, table string, id uuid.UUID) (Noder, 
 	case todo.Table:
 		n, err := c.Todo.Query().
 			Where(todo.ID(id)).
-			CollectFields(ctx, "Todo").
 			Only(ctx)
 		if err != nil {
 			return nil, err
@@ -343,7 +341,6 @@ func (c *Client) noders(ctx context.Context, table string, ids []uuid.UUID) ([]N
 	case category.Table:
 		nodes, err := c.Category.Query().
 			Where(category.IDIn(ids...)).
-			CollectFields(ctx, "Category").
 			All(ctx)
 		if err != nil {
 			return nil, err
@@ -356,7 +353,6 @@ func (c *Client) noders(ctx context.Context, table string, ids []uuid.UUID) ([]N
 	case todo.Table:
 		nodes, err := c.Todo.Query().
 			Where(todo.IDIn(ids...)).
-			CollectFields(ctx, "Todo").
 			All(ctx)
 		if err != nil {
 			return nil, err
