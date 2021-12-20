23ecd790436b6267fc2f09d8cc7e0fe1c360d7a1
MasseElch <12862103+masseelch@users.noreply.github.com>
2021-12-20 16:18:41 +0100

    all: upgrade ent to latest master (#183)

--- entproto/internal/todo/ent/runtime/runtime.go
+++ entproto/internal/todo/ent/runtime/runtime.go
@@ -5,6 +5,6 @@ package runtime
 // The schema-stitching logic is generated in entgo.io/contrib/entproto/internal/todo/ent/runtime.go
 
 const (
-	Version = "v0.9.2-0.20211125103743-6372263b990c"            // Version of ent codegen.
-	Sum     = "h1:cfHfo57DDwXOvYeQ3hbjaBy5IyoJcgpvMdtGNzhXR5w=" // Sum of ent codegen.
+	Version = "v0.9.2-0.20211220092907-4d01a56b8de7"            // Version of ent codegen.
+	Sum     = "h1:v5R9L5IE4fakNcs5OlNKFZ5Cn0Oa6ogs2o8qmHSWO4Y=" // Sum of ent codegen.
 )
