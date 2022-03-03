39a334e0dac9e87bc645a266138f10226fc41251
Shay <shay@bionicstork.com>
2022-03-03 17:08:01 +0200

    Enum converter fix

--- entproto/cmd/protoc-gen-entgrpc/converter.go
+++ entproto/cmd/protoc-gen-entgrpc/converter.go
@@ -64,7 +64,7 @@ func (g *serviceGenerator) newConverter(fld *entproto.FieldMappingDescriptor, no
 		}
 	case dpb.FieldDescriptorProto_TYPE_ENUM:
 		enumName := fld.PbFieldDescriptor.GetEnumType().GetName()
-		method := fmt.Sprintf("toProto%s_%s", nodeName, enumName)
+		method := fmt.Sprintf("ToProto%s_%s", nodeName, enumName)
 		out.ToProtoConstructor = g.File.GoImportPath.Ident(method)
 	case dpb.FieldDescriptorProto_TYPE_MESSAGE:
 		if fld.IsEdgeField {
