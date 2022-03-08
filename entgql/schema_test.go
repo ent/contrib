7dbb7a6f11bfe22ce4adffadea5433c4234eb251
Giau. Tran Minh <giau.tmg@gmail.com>
2022-03-09 00:22:22 +0700

    entgql: cleanup source code by copy content to a single file (#255)
    * entgql: fix todoplugin server imports

--- entgql/schema_test.go
+++ entgql/schema_test.go
@@ -15,10 +15,12 @@
 package entgql
 
 import (
+	"reflect"
 	"testing"
 
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
+	"entgo.io/ent/schema/field"
 	"github.com/stretchr/testify/require"
 	"github.com/vektah/gqlparser/v2/ast"
 )
@@ -261,6 +263,374 @@ enum VisibilityStatus @goModel(model: "entgo.io/contrib/entgql/internal/todoplug
 `, printSchema(&ast.Schema{
 		Types: types,
 	}))
+}
+
+func TestSchema_relayConnectionTypes(t *testing.T) {
+	type args struct {
+		t *gen.Type
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Todo",
+			args: args{
+				t: &gen.Type{
+					Name: "Todo",
+				},
+			},
+			want: `"""
+A connection to a list of items.
+"""
+type TodoConnection {
+	"""
+	A list of edges.
+	"""
+	edges: [TodoEdge]
+	"""
+	Information to aid in pagination.
+	"""
+	pageInfo: PageInfo!
+	totalCount: Int!
+}
+"""
+An edge in a connection.
+"""
+type TodoEdge {
+	"""
+	The item at the end of the edge.
+	"""
+	node: Todo
+	"""
+	A cursor for use in pagination.
+	"""
+	cursor: Cursor!
+}
+`,
+		},
+		{
+			name: "Todo_with_type",
+			args: args{
+				t: &gen.Type{
+					Name: "Todo",
+					Annotations: map[string]interface{}{
+						annotationName: map[string]interface{}{
+							"Type": "SuperTodo",
+						},
+					},
+				},
+			},
+			want: `"""
+A connection to a list of items.
+"""
+type SuperTodoConnection {
+	"""
+	A list of edges.
+	"""
+	edges: [SuperTodoEdge]
+	"""
+	Information to aid in pagination.
+	"""
+	pageInfo: PageInfo!
+	totalCount: Int!
+}
+"""
+An edge in a connection.
+"""
+type SuperTodoEdge {
+	"""
+	The item at the end of the edge.
+	"""
+	node: SuperTodo
+	"""
+	A cursor for use in pagination.
+	"""
+	cursor: Cursor!
+}
+`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := relayConnectionTypes(tt.args.t)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("relayConnection() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			s := &ast.Schema{
+				Types: map[string]*ast.Definition{},
+			}
+			insertDefinitions(s.Types, got...)
+			gots := printSchema(s)
+			if !reflect.DeepEqual(gots, tt.want) {
+				t.Errorf("relayConnection() = %v, want %v", gots, tt.want)
+			}
+		})
+	}
+}
+
+func TestSchema_relayBuiltinTypes(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "relayBuiltinTypes",
+			want: `"""
+Define a Relay Cursor type:
+https://relay.dev/graphql/connections.htm#sec-Cursor
+"""
+scalar Cursor
+"""
+An object with an ID.
+Follows the [Relay Global Object Identification Specification](https://relay.dev/graphql/objectidentification.htm)
+"""
+interface Node {
+	"""
+	The id of the object.
+	"""
+	id: ID!
+}
+"""
+Information about pagination in a connection.
+https://relay.dev/graphql/connections.htm#sec-undefined.PageInfo
+"""
+type PageInfo {
+	"""
+	When paginating forwards, are there more items?
+	"""
+	hasNextPage: Boolean!
+	"""
+	When paginating backwards, are there more items?
+	"""
+	hasPreviousPage: Boolean!
+	"""
+	When paginating backwards, the cursor to continue.
+	"""
+	startCursor: Cursor
+	"""
+	When paginating forwards, the cursor to continue.
+	"""
+	endCursor: Cursor
+}
+`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := relayBuiltinTypes()
+
+			s := &ast.Schema{
+				Types: map[string]*ast.Definition{},
+			}
+			insertDefinitions(s.Types, got...)
+			gots := printSchema(s)
+			if !reflect.DeepEqual(gots, tt.want) {
+				t.Errorf("relayBuiltinTypes() = %v, want %v", gots, tt.want)
+			}
+		})
+	}
+}
+
+func TestModifyConfig_empty(t *testing.T) {
+	e, err := newSchemaGenerator(&gen.Graph{
+		Config: &gen.Config{
+			Package: "example.com",
+		},
+	})
+	require.NoError(t, err)
+	e.relaySpec = false
+
+	cfg, err := e.genModels()
+	require.NoError(t, err)
+
+	expected := map[string]string{}
+	require.Equal(t, expected, cfg)
+}
+
+func TestModifyConfig(t *testing.T) {
+	e, err := newSchemaGenerator(createGraph(false))
+	require.NoError(t, err)
+
+	e.relaySpec = false
+	cfg, err := e.genModels()
+	require.NoError(t, err)
+	expected := map[string]string{
+		"Todo":          "example.com.Todo",
+		"Group":         "example.com.Group",
+		"GroupWithSort": "example.com.GroupWithSort",
+	}
+	require.Equal(t, expected, cfg)
+}
+
+func TestModifyConfig_relay(t *testing.T) {
+	g := createGraph(true)
+	e, err := newSchemaGenerator(g)
+	e.relaySpec = false
+	require.NoError(t, err)
+	_, err = e.genModels()
+	require.Error(t, err, ErrRelaySpecDisabled)
+
+	e.relaySpec = true
+	cfg, err := e.genModels()
+	require.NoError(t, err)
+	expected := map[string]string{
+		"Cursor":                  "example.com.Cursor",
+		"Group":                   "example.com.Group",
+		"GroupConnection":         "example.com.GroupConnection",
+		"GroupEdge":               "example.com.GroupEdge",
+		"GroupWithSort":           "example.com.GroupWithSort",
+		"GroupWithSortConnection": "example.com.GroupWithSortConnection",
+		"GroupWithSortEdge":       "example.com.GroupWithSortEdge",
+		"GroupWithSortOrder":      "example.com.GroupWithSortOrder",
+		"GroupWithSortOrderField": "example.com.GroupWithSortOrderField",
+		"Node":                    "example.com.Noder",
+		"OrderDirection":          "example.com.OrderDirection",
+		"PageInfo":                "example.com.PageInfo",
+		"Todo":                    "example.com.Todo",
+		"TodoConnection":          "example.com.TodoConnection",
+		"TodoEdge":                "example.com.TodoEdge",
+	}
+	require.Equal(t, expected, cfg)
+}
+
+func TestModifyConfig_todoplugin(t *testing.T) {
+	graph, err := entc.LoadGraph("./internal/todoplugin/ent/schema", &gen.Config{})
+	require.NoError(t, err)
+	disableRelayConnection(graph)
+
+	e, err := newSchemaGenerator(graph)
+	require.NoError(t, err)
+	e.relaySpec = false
+
+	cfg, err := e.genModels()
+	require.NoError(t, err)
+
+	expected := map[string]string{
+		"Category":         "entgo.io/contrib/entgql/internal/todoplugin/ent.Category",
+		"CategoryStatus":   "entgo.io/contrib/entgql/internal/todoplugin/ent/category.Status",
+		"CategoryConfig":   "entgo.io/contrib/entgql/internal/todo/ent/schema/schematype.CategoryConfig",
+		"MasterUser":       "entgo.io/contrib/entgql/internal/todoplugin/ent.User",
+		"Role":             "entgo.io/contrib/entgql/internal/todoplugin/ent/role.Role",
+		"Status":           "entgo.io/contrib/entgql/internal/todoplugin/ent/todo.Status",
+		"Todo":             "entgo.io/contrib/entgql/internal/todoplugin/ent.Todo",
+		"VisibilityStatus": "entgo.io/contrib/entgql/internal/todoplugin/ent/todo.VisibilityStatus",
+	}
+	require.Equal(t, expected, cfg)
+}
+
+func TestModifyConfig_todoplugin_relay(t *testing.T) {
+	graph, err := entc.LoadGraph("./internal/todoplugin/ent/schema", &gen.Config{})
+	require.NoError(t, err)
+
+	e, err := newSchemaGenerator(graph)
+	require.NoError(t, err)
+	cfg, err := e.genModels()
+	require.NoError(t, err)
+	expected := map[string]string{
+		"Category":             "entgo.io/contrib/entgql/internal/todoplugin/ent.Category",
+		"CategoryConfig":       "entgo.io/contrib/entgql/internal/todo/ent/schema/schematype.CategoryConfig",
+		"CategoryConnection":   "entgo.io/contrib/entgql/internal/todoplugin/ent.CategoryConnection",
+		"CategoryEdge":         "entgo.io/contrib/entgql/internal/todoplugin/ent.CategoryEdge",
+		"CategoryOrder":        "entgo.io/contrib/entgql/internal/todoplugin/ent.CategoryOrder",
+		"CategoryOrderField":   "entgo.io/contrib/entgql/internal/todoplugin/ent.CategoryOrderField",
+		"CategoryStatus":       "entgo.io/contrib/entgql/internal/todoplugin/ent/category.Status",
+		"Cursor":               "entgo.io/contrib/entgql/internal/todoplugin/ent.Cursor",
+		"MasterUser":           "entgo.io/contrib/entgql/internal/todoplugin/ent.User",
+		"MasterUserConnection": "entgo.io/contrib/entgql/internal/todoplugin/ent.MasterUserConnection",
+		"MasterUserEdge":       "entgo.io/contrib/entgql/internal/todoplugin/ent.MasterUserEdge",
+		"Node":                 "entgo.io/contrib/entgql/internal/todoplugin/ent.Noder",
+		"OrderDirection":       "entgo.io/contrib/entgql/internal/todoplugin/ent.OrderDirection",
+		"PageInfo":             "entgo.io/contrib/entgql/internal/todoplugin/ent.PageInfo",
+		"Role":                 "entgo.io/contrib/entgql/internal/todoplugin/ent/role.Role",
+		"Status":               "entgo.io/contrib/entgql/internal/todoplugin/ent/todo.Status",
+		"Todo":                 "entgo.io/contrib/entgql/internal/todoplugin/ent.Todo",
+		"TodoConnection":       "entgo.io/contrib/entgql/internal/todoplugin/ent.TodoConnection",
+		"TodoEdge":             "entgo.io/contrib/entgql/internal/todoplugin/ent.TodoEdge",
+		"TodoOrder":            "entgo.io/contrib/entgql/internal/todoplugin/ent.TodoOrder",
+		"TodoOrderField":       "entgo.io/contrib/entgql/internal/todoplugin/ent.TodoOrderField",
+		"VisibilityStatus":     "entgo.io/contrib/entgql/internal/todoplugin/ent/todo.VisibilityStatus",
+	}
+	require.Equal(t, expected, cfg)
+}
+
+func createGraph(relayConnection bool) *gen.Graph {
+	return &gen.Graph{
+		Config: &gen.Config{
+			Package: "example.com",
+			IDType: &field.TypeInfo{
+				Type: field.TypeInt,
+			},
+		},
+		Nodes: []*gen.Type{
+			{
+				Name: "Todo",
+				Fields: []*gen.Field{{
+					Name: "Name",
+					Type: &field.TypeInfo{
+						Type: field.TypeString,
+					},
+				}},
+				Annotations: map[string]interface{}{
+					annotationName: map[string]interface{}{
+						"RelayConnection": relayConnection,
+					},
+				},
+			},
+			{
+				Name: "User",
+				Fields: []*gen.Field{{
+					Name: "Name",
+					Type: &field.TypeInfo{
+						Type: field.TypeString,
+					},
+				}},
+				Annotations: map[string]interface{}{
+					annotationName: map[string]interface{}{
+						"Skip": true,
+					},
+				},
+			},
+			{
+				Name: "Group",
+				Fields: []*gen.Field{{
+					Name: "Name",
+					Type: &field.TypeInfo{
+						Type: field.TypeString,
+					},
+				}},
+				Annotations: map[string]interface{}{
+					annotationName: map[string]interface{}{
+						"RelayConnection": relayConnection,
+					},
+				},
+			},
+			{
+				Name: "GroupWithSort",
+				Fields: []*gen.Field{{
+					Name: "Name",
+					Type: &field.TypeInfo{
+						Type: field.TypeString,
+					},
+					Annotations: map[string]interface{}{
+						annotationName: map[string]interface{}{
+							"OrderField": "NAME",
+						},
+					},
+				}},
+				Annotations: map[string]interface{}{
+					annotationName: map[string]interface{}{
+						"RelayConnection": relayConnection,
+					},
+				},
+			},
+		},
+	}
 }
 
 func disableRelayConnection(g *gen.Graph) {
