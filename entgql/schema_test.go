32c47c2c8e4d5ac10946b3227769e8608ddd82a8
Christopher Schmitt <cschmitt@tankbusta.net>
2022-12-30 19:45:09 -0700

    entgql: prefer edge schema over normal edge rel

--- entgql/schema_test.go
+++ entgql/schema_test.go
@@ -238,7 +238,6 @@ type User {
   name: String!
   username: UUID!
   groups: [Group!]
-  friends: [User!]
   friendships: [Friendship!]
 }
 `, printSchema(schema))
@@ -566,6 +565,12 @@ input FriendshipWhereInput {
   createdAtGTE: Time
   createdAtLT: Time
   createdAtLTE: Time
+  """user edge predicates"""
+  hasUser: Boolean
+  hasUserWith: [UserWhereInput!]
+  """friend edge predicates"""
+  hasFriend: Boolean
+  hasFriendWith: [UserWhereInput!]
 }
 type Group implements Node @hasPermissions(permissions: ["ADMIN","MODERATOR"]) {
   id: ID!
@@ -945,22 +950,6 @@ type User implements Node {
     """Filtering options for Groups returned from the connection."""
     where: GroupWhereInput
   ): GroupConnection!
-  friends(
-    """Returns the elements in the list that come after the specified cursor."""
-    after: Cursor
-
-    """Returns the first _n_ elements from the list."""
-    first: Int
-
-    """Returns the elements in the list that come before the specified cursor."""
-    before: Cursor
-
-    """Returns the last _n_ elements from the list."""
-    last: Int
-
-    """Filtering options for Users returned from the connection."""
-    where: UserWhereInput
-  ): UserConnection!
   friendships(
     """Returns the elements in the list that come after the specified cursor."""
     after: Cursor
@@ -1037,9 +1026,6 @@ input UserWhereInput {
   """groups edge predicates"""
   hasGroups: Boolean
   hasGroupsWith: [GroupWhereInput!]
-  """friends edge predicates"""
-  hasFriends: Boolean
-  hasFriendsWith: [UserWhereInput!]
   """friendships edge predicates"""
   hasFriendships: Boolean
   hasFriendshipsWith: [FriendshipWhereInput!]
--- entgql/template.go
+++ entgql/template.go
@@ -334,6 +334,9 @@ func filterEdges(edges []*gen.Edge, skip SkipMode) ([]*gen.Edge, error) {
 		if e.Type.HasCompositeID() {
 			continue
 		}
+		if e.Through != nil && e.Through.IsEdgeSchema() {
+			continue
+		}
 		antE, err := annotation(e.Annotations)
 		if err != nil {
 			return nil, err
