adb8b9369eec31dd9c0dddb2d6773f9d551a793a
Dmitrii Tunikov <dmitrii@spread.ai>
2023-10-18 21:50:25 +0200

    add nodes placeholder for connection

--- entgql/template.go
+++ entgql/template.go
@@ -580,6 +580,11 @@ func (p *PaginationNames) TypeDefs() []*ast.Definition {
 					Type:        ast.ListType(ast.NamedType(p.Edge, nil), nil),
 					Description: "A list of edges.",
 				},
+				{
+					Name:        "nodes",
+					Type:        ast.ListType(ast.NamedType(p.Node, nil), nil),
+					Description: "A list of nodes.",
+				},
 				/*{
 					Name:        "pageInfo",
 					Type:        ast.NonNullNamedType(RelayPageInfo, nil),
